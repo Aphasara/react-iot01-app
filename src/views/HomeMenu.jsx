7dc59612426ec13523f70016375c27f7817c57d2
Aphasara <s6419410038@sau.ac.th>
2025-04-01 10:58:41 +0700

    V.2 Edit HomeMenu

--- src/views/HomeMenu.jsx
+++ src/views/HomeMenu.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Box, Typography, Button, Avatar } from '@mui/material'
 
 function HomeMenu() {
   return (
-    <div>
-        Aphasara Mahawan
-    </div>
+    <>
+        <Box sx={{height: '100vh', display: 'flex'}}>
+            <Box sx={{width: '70%', boxShadow: 2, mx: 'auto', my:'auto', p:3}}>
+                <Avatar src='https://img.securityinfowatch.com/files/base/cygnus/siw/image/2021/10/bigstock_Internet_Of_Things__Iot_Conce_413663834.6165ebd7685e3.png?auto=format,compress&fit=max&q=45&w=640&width=640'
+                sx={{ width: 150, height: 150, mx: 'auto'}} />
+                <Typography variant='h3' sx={{textAlign: 'center', mt: 2}}>
+                    IoT Calculate by Aphasara SAU
+                </Typography>
+            </Box>
+        </Box>
+    </>
   )
 }
 
